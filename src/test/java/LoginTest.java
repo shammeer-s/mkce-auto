19480443c4bd5b433c13d70823a85dd5c58017c2
Mohammed Shammeer <mohammedshammeer.s@gmail.com>
2026-04-27 14:17:52 +0530

    JIRA-1001 init

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -14,18 +14,10 @@ public class LoginTest {
     @BeforeMethod
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-
-
-
         options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080", "--no-sandbox", "--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-
-
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-
-
     }
 
     @Test
