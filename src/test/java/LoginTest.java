67ce2dfb3b1695560c8cddd5db7f6a2b11b76568
Mohammed Shammeer <mohammedshammeer.s@gmail.com>
2026-04-27 11:14:28 +0530

    Init

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080", "--no-sandbox", "--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void verifyApplicationLaunch() {
+        driver.get("https://the-internet.herokuapp.com/");
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(pageTitle.contains("The Internet"), "Title mismatch. Current title: " + pageTitle);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
